81852d6412f07aa7c5b862a9c575553e4d312abb
rustam sagaddinov <rustiksagad@gmail.com>
2025-05-02 22:04:00 -0300

    first commit

---
+++ src/main/java/com/utn/gestion_de_turnos/GestionDeTurnosApplication.java
@@ -0,0 +1,13 @@
+package com.utn.gestion_de_turnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeTurnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeTurnosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utn/gestion_de_turnos/GestionDeTurnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.gestion_de_turnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeTurnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

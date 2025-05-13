01c42d6fef6cee1f8c1b95e33097eced64fb18ee
luca884 <lucamarinio20@gmail.com>
2025-05-13 19:44:54 -0300

    lombok en clase sala y reserva

--- src/main/java/com/utn/gestion_de_turnos/model/Reserva.java
+++ src/main/java/com/utn/gestion_de_turnos/model/Reserva.java
@@ -1,10 +1,13 @@
 package com.utn.gestion_de_turnos.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
-
+@Entity
+@Table (name = "reserva" )
 public class Reserva {
 
     private int id;

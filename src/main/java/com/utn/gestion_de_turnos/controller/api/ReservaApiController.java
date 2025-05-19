8d71c87592b7e2c55d9f904cc02fb21b041e70f1
luca884 <lucamarinio20@gmail.com>
2025-05-18 21:58:09 -0300

    Implementacion de ReservaController con sus metodos principales

---
+++ src/main/java/com/utn/gestion_de_turnos/controller/api/ReservaApiController.java
@@ -0,0 +1,61 @@
+package com.utn.gestion_de_turnos.controller.api;
+
+
+import com.utn.gestion_de_turnos.model.Reserva;
+import com.utn.gestion_de_turnos.service.ReservaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reserva")
+public class ReservaApiController {
+
+    @Autowired
+    private ReservaService reservaService;
+
+    @PostMapping
+    public Reserva createReserva(@RequestBody Reserva reserva){
+        return reservaService.save(reserva);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Reserva> getReservaById(@PathVariable Long id){
+        return reservaService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteReservaById(@PathVariable Long id){
+        reservaService.deleteById(id);
+    }
+
+    @GetMapping
+    public List<Reserva> findAll(){
+       return reservaService.findAll();
+    }
+
+    @GetMapping("/estado")
+    public void findByEstado(Reserva.Estado estado){
+        reservaService.findByEstado(estado);
+    }
+
+    @PutMapping("/{id}/cancelar")
+    public void cancelarReserva(@PathVariable Long id){
+        reservaService.cancelarReserva(id);
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
